05668053e94a0f9e9299e203e9d63eda3784dfb1
Meehir <98355618+meehirbhalla@users.noreply.github.com>
2023-03-10 12:52:04 -0500

    edits and add click event listener to filter

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -5,6 +5,10 @@
 
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  const filterButton = document.querySelector('.filter_button')
+
+  let currentList = []; //scoped to main event function
+
   form.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
     submitEvent.preventDefault(); // This prevents your page from going to http://localhost:3000/api even if your form still has an action set on it
     console.log('form submission'); // this is substituting for a "breakpoint"
@@ -56,6 +60,11 @@ async function mainEvent() { // the async keyword means we can make API requests
     // arrayFromJson.data - we're accessing a key called 'data' on the returned object
     // it initially contains all 1,000 records from your request
   });
+
+  filterButton.addEventListener('click', (event) => {
+    // do not need to prevent default since it is not a submit button (GET and POST req)
+    console.log('clicked FilterButton');
+  })
 }
 
 /*
