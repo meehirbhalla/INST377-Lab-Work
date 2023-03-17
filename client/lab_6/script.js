2740f1cb4d01452ddd73229e1a644d34af6e4bcf
Meehir <98355618+meehirbhalla@users.noreply.github.com>
2023-03-17 11:49:29 -0400

    random int js function

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -11,6 +11,12 @@
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 */
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 function filterList(list, query) {
   return list.filter((item) => {
     const lowerCaseName = item.name.toLowerCase();
