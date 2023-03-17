0280c519a4a9cdb7eb45162dea587bda1cb681af
Meehir <98355618+meehirbhalla@users.noreply.github.com>
2023-03-17 11:43:53 -0400

    retain comments from lab 6 js template

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,3 +1,16 @@
+/* eslint-disable max-len */
+
+/*
+  Hook this script to index.html
+  by adding `<script src="script.js">` just before your closing `</body>` tag
+*/
+
+/*
+  ## Utility Functions
+    Under this comment place any utility functions you need - like an inclusive random number selector
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+*/
+
 function filterList(list, query) {
   return list.filter((item) => {
     const lowerCaseName = item.name.toLowerCase();
